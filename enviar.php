27d4cf0af29739be1607bbc0d36c4608d3f3ef4a
Brayan <ubrahian@gmail.com>
2018-07-17 18:38:10 -0500

    Comentario subiendo archivos

---
+++ enviar.php
@@ -0,0 +1,9 @@
+<?php
+    $destino = "ubrahian@gmail.com";
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $telefono = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
+    mail($destino, "Contacto", $contenido);
+?>
